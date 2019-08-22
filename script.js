4ad273e972023e76b24a0baea52649e3c9e0759d
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-08-22 12:08:02 +0200

    Change script.js

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var element = React.createElement('div', {}, 'Hello world!');
-ReactDom.render(elemt, document.getElementById('app'));
+var element = 
+    React.createElement('div', {},
+        React.createElement('h1', {}, 'Moves List'),
+        React.createElement('ul', {},
+            React.createElement('li', {},
+                React.createElement('h2', {}, 'Harry Potter'),
+                React.createElement('p', {}, 'Movie About Young  Wizzard')
+            ),
+        React.createElement('li', {},
+            React.createElement('h2', {}, 'Lion King'),
+            React.createElement('p', {}, 'Movie about king of jungle')
+            )    
+        )
+    );
