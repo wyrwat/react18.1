7d345c726063ab33bdaf17873861c4f78fec7f6c
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-08-22 12:45:47 +0200

    Change script.js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
+var movies = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        desc: 'Movie About Young  Wizzard',
+        img: 'images/harry.jpg'
+    },
+    {
+        id: 2,
+        title: 'Lion King',
+        desc: 'Movie about king of jungle',
+        img: 'images/krol.jpg'
+    }
+];
+
+var moviesElement = movies.map(function(movie){
+    return React.createElement('li', {key: movie.id},
+        React.createElement('h2', {}, movie.title),
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img', {src: movie.img})
+        );
+});
+
 var element = 
     React.createElement('div', {},
-        React.createElement('h1', {}, 'Moves List'),
-        React.createElement('ul', {},
-            React.createElement('li', {},
-                React.createElement('h2', {}, 'Harry Potter'),
-                React.createElement('p', {}, 'Movie About Young  Wizzard')
-            ),
-        React.createElement('li', {},
-            React.createElement('h2', {}, 'Lion King'),
-            React.createElement('p', {}, 'Movie about king of jungle')
-            )    
-        )
-    );
+        React.createElement('h1', {}, 'Movies List'),
+    	React.createElement('ul',{}, moviesElement)
+);
+
+ReactDOM.render(element, document.getElementById('app'));
