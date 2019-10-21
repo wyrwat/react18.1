872707357a31c25f7bbc4ae7e0bb4d73fb43c36e
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-10-21 14:15:54 +0200

    Add new component

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-var Movie = React.createClass({
-    propTypes: {
-        movies: React.PropTypes.object.isRequired,
-    },
-
-    render: function() {
-        return (
-            React.createElement('div', {},
-            React.createElement('h1', {}, 'Movies List'),
-            React.createElement('ul',{}, moviesElement)
-            )
-        )
-    },
-});
-
 var movies = [
     {
         id: 1,
         title: 'Harry Potter',
-        desc: 'Movie About Young  Wizzard',
+        desc: 'Movie About Young Wizzard',
         img: 'images/harry.jpg'
     },
     {
@@ -28,18 +13,39 @@ var movies = [
     }
 ];
 
-var moviesElement = movies.map(function(movie){
-    return React.createElement('li', {key: movie.id},
-        React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc),
-        React.createElement('img', {src: movie.img, alt: movie.title})
-        );
+var App = React.createClass({
+    render: function() {
+        return (
+            React.createElement('div', {className: 'app'},
+            React.createElement(MovieList, {movies: movies, header: 'Movies List'}),
+            )
+        )
+    },
 });
 
-/*var element = 
-    React.createElement('div', {},
-        React.createElement('h1', {}, 'Movies List'),
-    	React.createElement('ul',{}, moviesElement)
-);*/
-var element = React.createElement(Movie, {key: movies.id});
-ReactDOM.render(element, document.getElementById('app'));
+var MovieList = React.createClass({
+    propTypes: {
+        header: React.PropTypes.string.isRequired,
+        movies: React.PropTypes.array.isRequired,
+    },
+    
+    render: function() {
+        var moviesElement = this.props.movies.map(function(movie){
+            return React.createElement('li', {key: movie.id},
+                React.createElement('h2', {}, movie.title),
+                React.createElement('p', {}, movie.desc),
+                React.createElement('img', {src: movie.img, alt: movie.title})
+                );
+        });
+
+        return (
+        React.createElement('div', {}, 
+            React.createElement('h1',{className: 'header'}, this.props.header),
+            React.createElement('ul',{}, moviesElement)
+            )
+        )
+   } 
+});
+
+var app = React.createElement(App, {key: movies.id});
+ReactDOM.render(app, document.getElementById('app'));
